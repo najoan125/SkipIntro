f8fbcb06cde2fa093ac7480a23eda913bc5b238d
Najoan <68422289+najoan125@users.noreply.github.com>
2022-03-12 17:44:57 +0900

    초기 커밋.

---
+++ SkipIntro/Main.cs
@@ -0,0 +1,51 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityModManagerNet;
+
+namespace SkipIntro
+{
+    public static class Main
+    {
+        public static UnityModManager.ModEntry.ModLogger Logger;
+        public static Harmony harmony;
+        public static bool IsEnabled = false;
+        public static bool isplaying = false;
+
+        public static void Setup(UnityModManager.ModEntry modEntry)
+        {
+            Logger = modEntry.Logger;
+            modEntry.OnToggle = OnToggle;
+            modEntry.OnUpdate = OnUpdate;
+        }
+
+        private static void OnUpdate(UnityModManager.ModEntry modentry, float deltaTime)
+        {
+            if (!scrController.instance || !scrConductor.instance)
+            {
+                return;
+            }
+            isplaying = !scrController.instance.paused && scrConductor.instance.isGameWorld;
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            IsEnabled = value;
+
+            if (value)
+            {
+                harmony = new Harmony(modEntry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            else
+            {
+                harmony.UnpatchAll(modEntry.Info.Id);
+            }
+            return true;
+        }
+    }
+}
---
+++ SkipIntro/Patches.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SkipIntro
+{
+    public static class Patches
+    {
+        public static bool ChangeDeath = false;
+        [HarmonyPatch(typeof(scrConductor), "StartMusic")]
+        public static class StartMusicPatch
+        {
+            public static void Prefix()
+            {
+                if (scrController.deaths == 0 && Main.isplaying)
+                {
+                    scrController.deaths = 1;
+                    ChangeDeath = true;
+                    Main.Logger.Log("deaths = 1");
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(scrCountdown),"Update")]
+        public static class ShowGetReadyPatch
+        {
+            public static void Postfix(scrCountdown __instance)
+            {
+                if (ChangeDeath && __instance.controller.goShown)
+                {
+                    scrController.deaths = 0;
+                    ChangeDeath = false;
+                    Main.Logger.Log("deaths = 0");
+                }
+            }
+        }
+    }
+}
